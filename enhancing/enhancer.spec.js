bc5eb9b2ca09548837a92b8b6149c043bbc0f7b9
Drew Ross <mail.drew.ross@gmail.com>
2020-08-19 12:39:49 -0700

    Write and test repair() function.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,9 @@
 const enhancer = require('./enhancer.js');
+const { success, fail, repair, get } = require('./enhancer');
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 describe('success', () => {
 
@@ -9,7 +14,18 @@ describe('fail', () => {
 });
 
 describe('repair', () => {
+  it('returns an item object with durability: 100', () => {
+    const item = {
+      name: 'Item A',
+      durability: getRandomInt(99),
+      enhancement: getRandomInt(20)
+    };
+    expect(item.durability).not.toBe(100);
 
+    const repairedItem = repair(item);
+
+    expect(repairedItem.durability).toBe(100);
+  });
 });
 
 describe('get', () => {
